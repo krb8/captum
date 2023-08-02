c3bdb93798c494c8f3f37a880e7e4e985d893888
krb8 <134405824+krb8@users.noreply.github.com>
2023-08-02 16:16:12 -0400

    Add files via upload

---
+++ batch_launch.bat
@@ -0,0 +1,17 @@
+python attribution_method.py --image_dir C:\Users\krb8\PycharmProjects\captum\attributionOutput\POISONM\saliency\ --output_dirpath C:\Users\krb8\PycharmProjects\captum\attributio
+nOutput\POISONM\saliency
+
+python attribution_method.py --image_dir C:\Users\krb8\PycharmProjects\captum\attributionOutput\POISONM\deconvolution\ --output_dirpath C:\Users\krb8\PycharmProjects\captum\attributio
+nOutput\POISONM\deconvolution
+
+python attribution_method.py --image_dir C:\Users\krb8\PycharmProjects\captum\attributionOutput\POISONM\gradient_shap\ --output_dirpath C:\Users\krb8\PycharmProjects\captum\attributio
+nOutput\POISONM\gradient_shap
+
+python attribution_method.py --image_dir C:\Users\krb8\PycharmProjects\captum\attributionOutput\POISONM\guided_backprop\ --output_dirpath C:\Users\krb8\PycharmProjects\captum\attributio
+nOutput\POISONM\guided_backprop
+
+python attribution_method.py --image_dir C:\Users\krb8\PycharmProjects\captum\attributionOutput\POISONM\inputxgrad\ --output_dirpath C:\Users\krb8\PycharmProjects\captum\attributio
+nOutput\POISONM\inputxgrad
+
+python attribution_method.py --image_dir C:\Users\krb8\PycharmProjects\captum\attributionOutput\POISONM\integrated_grad\ --output_dirpath C:\Users\krb8\PycharmProjects\captum\attributio
+nOutput\POISONM\integrated_grad
